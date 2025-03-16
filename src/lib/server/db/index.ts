d565811d30e29972bad5d87245b38dd3b9866f02
vlakondra <kondrashkin.wladimir@yandex.ru>
2025-03-16 20:57:10 +0500

    `Added import of process.env and commented out DATABASE_URL check`

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,6 +2,8 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+// или process.env.
+
 //if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 const client = new Database('src/lib/server/db/auth.db')//env.DATABASE_URL);
 
